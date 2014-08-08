3d47ab68f4acc3dcd2f90891ebf3e090e81dd273
Cristina <chr1235@gmail.com>
2014-08-08 19:05:09 +0200

    Versión final

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php
 function set_count($file_name = 'counter.txt'){
 	if(file_exists($file_name)){
-
+		$count = (int) file_get_contents($file_name) + 1;
+		file_put_contents($file_name, $count);
 	}else{
 		$handle = fopen($file_name, 'w+');
-		fwrite($handle, 1)
+		$count = 1;
+		fwrite($handle, $count);
 		fclose($handle);
 	}
+
+	return $count;	
 }
 
-set_count();
+$count = set_count();
 
 require 'index.tmpl.php';
---
+++ index.tmpl.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1>Fan Website</h1>
+	<p>Visitor Count: <?php echo $count; ?>.</p>
+</body>
+</html>
